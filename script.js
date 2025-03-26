20149166637bf665366d4094c0d53ba9afa88612
M Haidar Hanif <git@mhaidarhanif.com>
2025-03-26 23:20:11 +0700

    feat: display contacts data

---
+++ script.js
@@ -0,0 +1,14 @@
+const contacts = [
+  {
+    name: "M Haidar Hanif",
+    email: "haidar@example.com",
+    phone: "+62-1234-56789",
+  },
+  {
+    name: "Azri Cakra",
+    email: "azricakra@example.com",
+    phone: "+62-4321-56789",
+  },
+];
+
+console.log(contacts);
