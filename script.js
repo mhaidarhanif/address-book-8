474e1b79deea9b8385ba33b72e5c11a6aaf01592
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-05 15:39:10 +0700

    display contacts data

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const contacts = [
   {
+    id: 1,
     name: "M Haidar Hanif",
+    age: 30,
     email: "haidar@example.com",
-    phone: "+62-1234-56789",
+    phone: "+62-851-0193029",
   },
   {
-    name: "Azri Cakra",
-    email: "azricakra@example.com",
-    phone: "+62-4321-56789",
+    id: 2,
+    name: "Elon Musk",
+    age: 50,
+    email: "elon@example.com",
+    phone: "+1-12121-0193029",
   },
 ];
 
