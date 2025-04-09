b0e84155ccf8e3e4141e77c6569639b9b3762da6
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-09 22:03:40 +0700

    display contacts with function & for loop

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const contacts = [
     age: 30,
     email: "haidar@example.com",
     phone: "+62-851-0193029",
+    isFavorited: true,
+    address: "Bandung, Indonesia",
   },
   {
     id: 2,
@@ -12,7 +14,23 @@ const contacts = [
     age: 50,
     email: "elon@example.com",
     phone: "+1-12121-0193029",
+    address: "California, USA",
   },
 ];
 
-console.log(contacts);
+function displayContacts() {
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+
+    console.log(`
+👤 Name: ${contact.name}
+📧 Email: ${contact.email}
+📞 Phone: ${contact.phone}
+🎂 Age: ${contact.age} years old
+📍 Address: ${contact.address}
+${contact.isFavorited ? "⭐ Favorited" : ""}
+`);
+  }
+}
+
+displayContacts();
